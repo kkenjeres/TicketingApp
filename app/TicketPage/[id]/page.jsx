75b8ccba4d87c319c9ea6490f678fa3d11d8acae
kkenjeres <kkenjeres96@gmail.com>
2024-02-07 20:31:25 +0100

    connect DB, dynamic data

--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -1,5 +1,7 @@
-const TicketPage = ({params}) => {
-  return <div>TicketPage {params.id}</div>;
+import TicketForm from "@/app/(components)/TicketForm";
+
+const TicketPage = ({ params }) => {
+  return <TicketForm />;
 };
 
 export default TicketPage;
