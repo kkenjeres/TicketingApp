6f2e16198cbf1efc1024439e51df6958e135b573
kkenjeres <kkenjeres96@gmail.com>
2024-02-07 21:06:19 +0100

    delete func

--- app/(components)/DeleteBlock.js
+++ app/(components)/DeleteBlock.js
@@ -1,9 +1,24 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import { FaXmark } from "react-icons/fa6";
 
-const DeleteBlock = () => {
+const DeleteBlock = ({ id }) => {
+  const router = useRouter();
+  const deleteTicket = async () => {
+    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+      method: "DELETE",
+    });
+    if (res.ok) {
+      router.refresh();
+    }
+  };
   return (
-    <FaXmark className="text-red-400 hover:cursor-pointer hover:text-red-200"/>
-  )
-}
+    <FaXmark
+      className="text-red-400 hover:cursor-pointer hover:text-red-200"
+      onClick={deleteTicket}
+    />
+  );
+};
 
-export default DeleteBlock
+export default DeleteBlock;
